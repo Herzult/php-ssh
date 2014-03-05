f4c6a91773977e16c40476f6047d7b1b59f7ff0e
Julius Beckmann <github@h4cc.de>
2014-03-05 12:49:14 +0100

    Added some tests for Exec. Somehow the STDERR Stream does not seem to work. Skipped that test so far.

---
+++ tests/Ssh/FunctionalTests/ExecTest.php
@@ -0,0 +1,63 @@
+<?php
+
+
+namespace Ssh\FunctionalTests;
+
+
+use Ssh\Authentication\Password;
+use Ssh\Configuration;
+use Ssh\Session;
+
+/**
+ * @author Julius Beckmann
+ *
+ * @group functional
+ */
+class ExecTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testExecuteWhoami()
+    {
+        $configuration = new Configuration('localhost');
+        $authentication = new Password(TEST_USER, TEST_PASSWORD);
+        $session = new Session($configuration, $authentication);
+
+        $exec = $session->getExec();
+        $output = $exec->run('whoami');
+
+        $this->assertEquals(TEST_USER, trim($output));
+    }
+
+    public function testExecuteMultilineOutput()
+    {
+        $configuration = new Configuration('localhost');
+        $authentication = new Password(TEST_USER, TEST_PASSWORD);
+        $session = new Session($configuration, $authentication);
+
+        $exec = $session->getExec();
+        $output = $exec->run('echo -e "a\nb\nc"');
+
+        // In case our SystemUnderTest differs
+        $output = str_replace("\r\n", "\n", $output);
+
+        $this->assertEquals("a\nb\nc\n", $output);
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testExecuteErrorOutput()
+    {
+        $this->markTestSkipped('TODO: This does not seem to work. Cant find whats wrong.');
+
+        $configuration = new Configuration('localhost');
+        $authentication = new Password(TEST_USER, TEST_PASSWORD);
+        $session = new Session($configuration, $authentication);
+
+        $exec = $session->getExec();
+        $output = $exec->run('cd /hom', true);
+
+        $this->assertEquals('', trim($output));
+    }
+}
+ 
